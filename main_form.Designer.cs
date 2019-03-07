a1b9bc22d31eb650e8b11f09ed8c93ed6131d70f
Ahmed El-Naggar <47918556+ahmedelnaggar1@users.noreply.github.com>
2019-03-07 23:02:30 +1000

    Assigned appropriate names to controls

--- main_form.Designer.cs
+++ main_form.Designer.cs
@@ -28,77 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.app_nameLbl = new System.Windows.Forms.Label();
-            this.app_nameTxtBox = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.dllLabel = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.injectBtn = new System.Windows.Forms.Button();
+            this.appListBox = new System.Windows.Forms.ListBox();
+            this.dllTxtBox = new System.Windows.Forms.TextBox();
+            this.selectBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // app_nameLbl
+            // injectBtn
             // 
-            this.app_nameLbl.AutoSize = true;
-            this.app_nameLbl.Location = new System.Drawing.Point(49, 27);
-            this.app_nameLbl.Name = "app_nameLbl";
-            this.app_nameLbl.Size = new System.Drawing.Size(90, 13);
-            this.app_nameLbl.TabIndex = 0;
-            this.app_nameLbl.Text = "Application Name";
+            this.injectBtn.Location = new System.Drawing.Point(479, 216);
+            this.injectBtn.Name = "injectBtn";
+            this.injectBtn.Size = new System.Drawing.Size(75, 33);
+            this.injectBtn.TabIndex = 0;
+            this.injectBtn.Text = "Inject";
+            this.injectBtn.UseVisualStyleBackColor = true;
             // 
-            // app_nameTxtBox
+            // appListBox
             // 
-            this.app_nameTxtBox.Location = new System.Drawing.Point(52, 43);
-            this.app_nameTxtBox.Name = "app_nameTxtBox";
-            this.app_nameTxtBox.Size = new System.Drawing.Size(114, 20);
-            this.app_nameTxtBox.TabIndex = 1;
+            this.appListBox.FormattingEnabled = true;
+            this.appListBox.Location = new System.Drawing.Point(12, 39);
+            this.appListBox.Name = "appListBox";
+            this.appListBox.Size = new System.Drawing.Size(542, 173);
+            this.appListBox.TabIndex = 1;
             // 
-            // textBox1
+            // dllTxtBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(52, 107);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(114, 20);
-            this.textBox1.TabIndex = 3;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.dllTxtBox.Location = new System.Drawing.Point(12, 13);
+            this.dllTxtBox.Name = "dllTxtBox";
+            this.dllTxtBox.Size = new System.Drawing.Size(463, 20);
+            this.dllTxtBox.TabIndex = 2;
             // 
-            // dllLabel
+            // selectBtn
             // 
-            this.dllLabel.AutoSize = true;
-            this.dllLabel.Location = new System.Drawing.Point(49, 91);
-            this.dllLabel.Name = "dllLabel";
-            this.dllLabel.Size = new System.Drawing.Size(71, 13);
-            this.dllLabel.TabIndex = 2;
-            this.dllLabel.Text = "DLL Location";
-            this.dllLabel.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(172, 107);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(63, 20);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Browse";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(106, 144);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(102, 40);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Inject";
-            this.button2.UseVisualStyleBackColor = true;
+            this.selectBtn.Location = new System.Drawing.Point(481, 11);
+            this.selectBtn.Name = "selectBtn";
+            this.selectBtn.Size = new System.Drawing.Size(73, 23);
+            this.selectBtn.TabIndex = 3;
+            this.selectBtn.Text = "Select DLL";
+            this.selectBtn.UseVisualStyleBackColor = true;
             // 
             // main_form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(304, 196);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.dllLabel);
-            this.Controls.Add(this.app_nameTxtBox);
-            this.Controls.Add(this.app_nameLbl);
+            this.ClientSize = new System.Drawing.Size(566, 272);
+            this.Controls.Add(this.selectBtn);
+            this.Controls.Add(this.dllTxtBox);
+            this.Controls.Add(this.appListBox);
+            this.Controls.Add(this.injectBtn);
             this.Name = "main_form";
             this.Text = "DLL Injector";
             this.ResumeLayout(false);
@@ -108,12 +85,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Label app_nameLbl;
-        private System.Windows.Forms.TextBox app_nameTxtBox;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label dllLabel;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button injectBtn;
+        private System.Windows.Forms.ListBox appListBox;
+        private System.Windows.Forms.TextBox dllTxtBox;
+        private System.Windows.Forms.Button selectBtn;
     }
 }
 
